8dde0ba86dde92902ceb9b6b9121b28c0bea6696
Athan Clark <athan.clark@gmail.com>
2018-07-25 17:53:47 -0600

    fixing overlap

--- src/LocalCooking/Template.hs
+++ src/LocalCooking/Template.hs
@@ -209,7 +209,7 @@ body {
 .loader:before {
   width: 5.2em;
   height: 10.2em;
-  background: #ffffff;
+  background: #{localCookingColorsMain};
   border-radius: 10.2em 0 0 10.2em;
   top: -0.1em;
   left: -0.1em;
